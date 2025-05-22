7ae84db6c2355da7c3473589892061768eb97395
gneL <binglengdegneL@gmail.com>
2025-05-22 16:45:44 +0800

    增加类注释、资源声明

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-NEW_VER="$1"                 # 用法：bash scripts/release.sh 0.0.3
+NEW_VER="$1"                 # 用法：bash scripts/release.sh 0.0.9
 
 git switch main
 git pull --rebase origin main
